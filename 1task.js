ca4fd27919f8839c26afd038bbb0ffcdbaa74723
Jacek Krużycki <kruzyk@gmail.com>
2018-08-23 23:23:17 +0200

    add correct names and shorter code

--- 1task.js
+++ 1task.js
@@ -1,7 +1,7 @@
 var BankAccount = 500;
-const PhonePrice = 50;
-const AccessoryPrice = 9.99;
-var TotalPrice = PhonePrice + AccessoryPrice;
+const PHONE_PRICE = 50;
+const ACCESSORY_PRICE = 9.99;
+var TotalPrice = PHONE_PRICE + ACCESSORY_PRICE;
 var TotalSpend = 0;
 
 while (BankAccount >= TotalPrice) {
@@ -10,6 +10,6 @@ while (BankAccount >= TotalPrice) {
     console.log("Money on my account:");
     console.log(BankAccount);
     console.log("Money spent:");
-    TotalSpend = TotalSpend + PhonePrice + AccessoryPrice;
+    TotalSpend = TotalSpend + TotalPrice;
     console.log(TotalSpend);
 }
