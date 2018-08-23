63c15344dc48cddf6f18ddaaad15a232b19257fa
Jacek Krużycki <kruzyk@gmail.com>
2018-08-23 23:18:34 +0200

    complete calculator

--- 1task.js
+++ 1task.js
@@ -1,9 +1,15 @@
 var BankAccount = 500;
 const PhonePrice = 50;
 const AccessoryPrice = 9.99;
-
 var TotalPrice = PhonePrice + AccessoryPrice;
+var TotalSpend = 0;
 
-while (PhonePrice <= BankAccount) {
-
+while (BankAccount >= TotalPrice) {
+    console.log("I buy a phone and accessories!");
+    BankAccount = BankAccount - TotalPrice;
+    console.log("Money on my account:");
+    console.log(BankAccount);
+    console.log("Money spent:");
+    TotalSpend = TotalSpend + PhonePrice + AccessoryPrice;
+    console.log(TotalSpend);
 }
