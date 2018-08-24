50bcfc9b21eca8358d9af4352b93e1c8c426b272
Jacek Krużycki <35242828+kruzyk@users.noreply.github.com>
2018-08-24 02:34:31 +0200

    fix variables names

--- 1task.js
+++ 1task.js
@@ -1,15 +1,15 @@
-var BankAccount = 500;
+var bank_account = 500;
 const PHONE_PRICE = 50;
 const ACCESSORY_PRICE = 9.99;
-var TotalPrice = PHONE_PRICE + ACCESSORY_PRICE;
-var TotalSpend = 0;
+var total_price = PHONE_PRICE + ACCESSORY_PRICE;
+var total_spend = 0;
 
-while (BankAccount >= TotalPrice) {
+while (bank_account >= total_price) {
     console.log("I buy a phone and accessories!");
-    BankAccount = BankAccount - TotalPrice;
+    BankAccount = bank_account - total_price;
     console.log("Money on my account:");
-    console.log(BankAccount);
+    console.log(bank_account);
     console.log("Money spent:");
-    TotalSpend = TotalSpend + TotalPrice;
-    console.log(TotalSpend);
+    TotalSpend = total_spend + total_price;
+    console.log(total_spend);
 }
