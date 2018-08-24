e8215c605469dd21736de9b31e32188ac2e5737b
Jacek Krużycki <kruzyk@gmail.com>
2018-08-24 03:03:11 +0200

    JS#7 JS variables

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
-alert("Hello World!");
-// one line comment
- 
-/* multiple
-lines
-comment */ 
+/* to create variable names we can't start with number like 1myVariable
+and we have to start with a lowercase letter */
+var myVariable_1;
+// we can define value like:
+myVariable_1 = 10;
+var myVar2 = "Hello";
+// we can change variable value in every time
+myVar2 = "hallo again";
+
